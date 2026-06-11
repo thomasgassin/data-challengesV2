100244a79864e2b9c87c49500e64005d491064dd
thomasgassin <thomas.gassin@gmail.com>
2026-06-11 22:09:48 +0200

    mise à jour

--- 01-Python/01-Programming-Basics/03-Data_Structures/currencies.py
+++ 01-Python/01-Programming-Basics/03-Data_Structures/currencies.py
@@ -1,12 +1,12 @@
 # pylint: disable=missing-docstring
 
 # TODO: add some currency rates
-RATES = {}
+RATES = { `USDEUR`: 0.85 , `GBPEUR`: 1.13 , `CHFEUR`: 0.86 , `EURGBP`: 0.885}
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-
+    amout = dict(amount[0] * dict[amout[1]+currency])
     return (amout,currency)  # YOUR CODE HERE
